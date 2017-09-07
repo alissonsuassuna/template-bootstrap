4994373e49712e3a0ae4dd195e4e548325f7bbd1
Alisson Suassuna <alissonsuassuna@gmail.com>
2017-09-07 13:34:19 -0300

    Finalizado

--- navbar-animation-fix.js
+++
@@ -1,8 +0,0 @@
-$('#collapse-navbar').on('show.bs.collapse', function(){
-    $('.header-banner').css('transform', 'translate(-50%, 10%)');
-});
-
-
-$('#collapse-navbar').on('hide.bs.collapse', function(){
-    $('.header-banner').css('transform', 'translate(-50%, -50%)');
-});;
